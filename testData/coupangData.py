293acf98353f96aeac6abf1b85c9ba98bfa1ea45
shehdgus <shehdgus0919@gmail.com>
2024-10-16 08:33:48 +0900

    데이터 수정

--- testData/coupangData.py
+++ testData/coupangData.py
@@ -1,9 +1,9 @@
 class coupnangData:
 
     test_coupang_data = [{
-        "ID":"shehdgus0919@naver.com",
-        "PW":"dkflrktma2@E", 
+        "ID":"myid@naver.com",
+        "PW":"MYPW123!@", 
         "이름" : "노동현",
-        "주소": "서울특별시 구로구 고척동 334 벽산블루밍아파트  206동 1601호",
-        "연락처":"010-4240-0919",
+        "주소": "서울특별시 OO구 OO동 OO아파트 000동 000호",
+        "연락처":"010-0000-0000",
     }]
